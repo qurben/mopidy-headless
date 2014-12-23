f936aec78c0f27c243ece0679eaf28867aa4bb7e
Gerben Oolbekkink <g.j.w.oolbekkink@gmail.com>
2014-12-23 23:57:25 +0100

    New config.
    Only one device, all basic controls

--- mopidy_headless/__init__.py
+++ mopidy_headless/__init__.py
@@ -19,12 +19,16 @@ class Extension(ext.Extension):
 
     def get_config_schema(self):
         schema = super(Extension, self).get_config_schema()
-        schema['volume_device'] = config.String()
-        schema['volume_axis'] = config.String()
-        schema['playlist_device'] = config.String()
-        schema['playlist_axis'] = config.String()
-        schema['mute_device'] = config.String()
-        schema['mute_key'] = config.String()
+        schema['device'] = config.String()
+        schema['volume_up'] = config.String()
+        schema['volume_down'] = config.String()
+        schema['next_song'] = config.String()
+        schema['previous_song'] = config.String()
+        schema['next_playlist'] = config.String()
+        schema['previous_playlist'] = config.String()
+        schema['play'] = config.String()
+        schema['pause'] = config.String()
+        schema['mute'] = config.String()
         return schema
 
     def setup(self, registry):
--- mopidy_headless/frontend.py
+++ mopidy_headless/frontend.py
@@ -11,29 +11,9 @@ from .input import InputThread, Handler
 
 logger = logging.getLogger(__name__)
 
-class VolumeHandler(Handler):
-  def __init__(self, device_fn, event_type, event_code, actor_ref):
-    super(VolumeHandler, self).__init__(device_fn, event_type, event_code)
-    self.actor_proxy=actor_ref.proxy()
-
-  def handle(self, event):
-    logger.debug("Volume change {0}".format(event.value))
-    self.actor_proxy.change_volume(event.value)
-
-
-class PlaylistHandler(Handler):
-  def __init__(self, device_fn, event_type, event_code, actor_ref):
-    super(PlaylistHandler, self).__init__(device_fn, event_type, event_code)
-    self.actor_proxy=actor_ref.proxy()
-
-  def handle(self, event):
-    logger.debug("Playlist change {0}".format(event.value))
-    self.actor_proxy.change_playlist(event.value)
-
-
-class MuteHandler(Handler):
-  def __init__(self, device_fn, event_type, event_code, actor_ref, longpress=5):
-    super(MuteHandler, self).__init__(device_fn, event_type, event_code)
+class KeyHandler(Handler):
+  def __init__(self, device_fn, event_code, actor_ref, longpress=5):
+    super(KeyHandler, self).__init__(device_fn, "EV_KEY", event_code)
     self.actor_proxy=actor_ref.proxy()
     self.longpress=longpress
 
@@ -43,29 +23,72 @@ class MuteHandler(Handler):
     if (event.value==0):
       diff=event.sec-self.timestamp
       if (diff>self.longpress):
-        logger.debug("Longpress mute")
-        self.actor_proxy.halt()
+        self.longpress(self.actor_proxy)
       else:
-        self.actor_proxy.toggle_mute()
+        self.press(self.actor_proxy)
 
+class VolumeUpHandler(KeyHandler):
+  def press(self, actor_proxy):
+    actor_proxy.change_volume(5)
+
+class VolumeDownHandler(KeyHandler):
+  def press(self, actor_proxy):
+    actor_proxy.change_volume(-5)
+
+class NextSongHandler(KeyHandler):
+  def press(self, actor_proxy):
+    actor_proxy.next_song()
+
+class PreviousSongHandler(KeyHandler):
+  def press(self, actor_proxy):
+    actor_proxy.previous_song()
+
+class NextPlaylistHandler(KeyHandler):
+  def press(self, actor_proxy):
+    actor_proxy.next_playlist()
+
+class PreviousPlaylistHandler(KeyHandler):
+  def press(self, actor_proxy):
+    actor_proxy.previous_playlist()
+
+class PlayHandler(KeyHandler):
+  def press(self, actor_proxy):
+    actor_proxy.play()
+
+class PauseHandler(KeyHandler):
+  def press(self, actor_proxy):
+    actor_proxy.pause()
+
+class MuteHandler(KeyHandler):
+  def press(self, actor_proxy):
+    actor_proxy.toggle_mute()
+
+  def longpress(self, actor_proxy):
+    actor_proxy.halt()
 
 class InputFrontend(pykka.ThreadingActor, core.CoreListener):
     def __init__(self, config, core):
         super(InputFrontend, self).__init__()
         self.config = config['headless']
         self.core=core
-        
-        self.playlists = None
-        self.selected_playlist = None
-        
 
+        self.selected_playlist = None
+        self.playlists = None
+        
     def on_start(self):
         self.playlists_loaded()
+        device = self.config["device"]
         
         self.inputthread=InputThread()
-        self.inputthread.registerHandler(VolumeHandler(self.config["volume_device"], "EV_REL", self.config["volume_axis"], self.actor_ref))
-        self.inputthread.registerHandler(PlaylistHandler(self.config["playlist_device"], "EV_REL", self.config["playlist_axis"], self.actor_ref))
-        self.inputthread.registerHandler(MuteHandler(self.config["mute_device"], "EV_KEY", self.config["mute_key"], self.actor_ref))
+        self.inputthread.registerHandler(PlayHandler(device, self.config["play"], self.actor_ref))
+        self.inputthread.registerHandler(PauseHandler(device, self.config["pause"], self.actor_ref))
+        self.inputthread.registerHandler(VolumeUpHandler(device, self.config["volume_up"], self.actor_ref))
+        self.inputthread.registerHandler(VolumeDownHandler(device, self.config["volume_down"], self.actor_ref))
+        self.inputthread.registerHandler(NextSongHandler(device, self.config["next_song"], self.actor_ref))
+        self.inputthread.registerHandler(PreviousSongHandler(device, self.config["previous_song"], self.actor_ref))
+        self.inputthread.registerHandler(NextPlaylistHandler(device, self.config["next_playlist"], self.actor_ref))
+        self.inputthread.registerHandler(PreviousPlaylistHandler(device, self.config["previous_playlist"], self.actor_ref))
+        self.inputthread.registerHandler(MuteHandler(device, self.config["mute"], self.actor_ref))
         self.inputthread.start()
 
     def on_stop(self):
@@ -83,21 +106,35 @@ class InputFrontend(pykka.ThreadingActor, core.CoreListener):
           
       logger.debug("Volume changed: {0}".format(volume))
       self.core.playback.volume=volume
+    
+    def next_song(self):
+        self.core.playback.next()
+
+    def previous_song(self):
+        self.core.playback.previous()
+
+    def change_playlist(self, value):
+        self.selected_playlist = (self.selected_playlist + value) % len(self.playlists)
+        self.core.tracklist.clear()
+        self.core.tracklist.add(uri=self.playlists[self.selected_playlist].uri)
+
+    def next_playlist(self):
+        self.change_playlist(1)
+
+    def previous_playlist(self):
+        self.change_playlist(-1)
+
+    def play(self):
+        self.core.playback.play()
+
+    def pause(self):
+        self.core.playback.pause()
 
     def toggle_mute(self):
         mute = not self.core.playback.mute.get()
         logger.debug("Muted: {0}".format(mute))
         self.core.playback.mute = mute
 
-    def change_playlist(self, value):
-        # Only change playlists when it is possible
-        if len(self.playlists) > 0:
-          self.selected_playlist= (self.selected_playlist+1) % len(self.playlists)
-          logger.debug("Change playlist: {0}".format(self.selected_playlist))
-          self.core.tracklist.clear()
-          self.core.tracklist.add(uri=self.playlists[self.selected_playlist].uri)
-          self.core.playback.play()
-        
     def playlists_loaded(self):
         self.playlists = []
         for playlist in self.core.playlists.playlists.get():
