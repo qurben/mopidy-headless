57719af90b2603564e3d52b40836a144f2420669
Gerben Oolbekkink <g.j.w.oolbekkink@gmail.com>
2014-12-24 15:10:37 +0100

    Make code PEP 8

--- mopidy_headless/frontend.py
+++ mopidy_headless/frontend.py
@@ -1,4 +1,3 @@
-
 from __future__ import unicode_literals
 
 import logging
@@ -11,107 +10,113 @@ from .input import InputThread, Handler
 
 logger = logging.getLogger(__name__)
 
-class KeyHandler(Handler):
-  def __init__(self, device_fn, event_code, actor_ref, longpress=5):
-    super(KeyHandler, self).__init__(device_fn, "EV_KEY", event_code)
-    self.actor_proxy=actor_ref.proxy()
-    self.longpress=longpress
 
-  def handle(self, event):
-    if (event.value==1):
-      self.timestamp=event.sec
-    if (event.value==0):
-      diff=event.sec-self.timestamp
-      if (diff>self.longpress):
-        self.longpress(self.actor_proxy)
-      else:
-        self.press(self.actor_proxy)
+class KeyHandler(Handler):
+    def __init__(self, device_fn, event_code, actor_ref, longpress=5):
+        super(KeyHandler, self).__init__(device_fn, "EV_KEY", event_code)
+        self.actor_proxy = actor_ref.proxy()
+        self.longpress = longpress
+        self.timestamp = None
+
+    def handle(self, event):
+        if event.value == 0:
+            self.press(self.actor_proxy)
+
+    def press(self, actor_proxy):
+        pass
+
 
 class VolumeUpHandler(KeyHandler):
-  def press(self, actor_proxy):
-    actor_proxy.change_volume(5)
+    def press(self, actor_proxy):
+        actor_proxy.change_volume(5)
+
 
 class VolumeDownHandler(KeyHandler):
-  def press(self, actor_proxy):
-    actor_proxy.change_volume(-5)
+    def press(self, actor_proxy):
+        actor_proxy.change_volume(-5)
+
 
 class NextSongHandler(KeyHandler):
-  def press(self, actor_proxy):
-    actor_proxy.next_song()
+    def press(self, actor_proxy):
+        actor_proxy.next_song()
+
 
 class PreviousSongHandler(KeyHandler):
-  def press(self, actor_proxy):
-    actor_proxy.previous_song()
+    def press(self, actor_proxy):
+        actor_proxy.previous_song()
+
 
 class NextPlaylistHandler(KeyHandler):
-  def press(self, actor_proxy):
-    actor_proxy.next_playlist()
+    def press(self, actor_proxy):
+        actor_proxy.next_playlist()
+
 
 class PreviousPlaylistHandler(KeyHandler):
-  def press(self, actor_proxy):
-    actor_proxy.previous_playlist()
+    def press(self, actor_proxy):
+        actor_proxy.previous_playlist()
+
 
 class PlayHandler(KeyHandler):
-  def press(self, actor_proxy):
-    actor_proxy.play()
+    def press(self, actor_proxy):
+        actor_proxy.play()
+
 
 class PauseHandler(KeyHandler):
-  def press(self, actor_proxy):
-    actor_proxy.pause()
+    def press(self, actor_proxy):
+        actor_proxy.pause()
+
 
 class PlayPauseHandler(KeyHandler):
-  def press(self, actor_proxy):
-    actor_proxy.playpause_toggle()
+    def press(self, actor_proxy):
+        actor_proxy.playpause_toggle()
+
 
 class MuteHandler(KeyHandler):
-  def press(self, actor_proxy):
-    actor_proxy.toggle_mute()
+    def press(self, actor_proxy):
+        actor_proxy.toggle_mute()
 
-  def longpress(self, actor_proxy):
-    actor_proxy.halt()
 
 class InputFrontend(pykka.ThreadingActor, core.CoreListener):
     def __init__(self, config, core):
         super(InputFrontend, self).__init__()
         self.config = config['headless']
-        self.core=core
+        self.core = core
 
         self.selected_playlist = None
         self.playlists = None
-        
+        self.inputthread = None
+
     def on_start(self):
         self.playlists_loaded()
         device = self.config["device"]
-        
-        self.inputthread=InputThread()
-        self.inputthread.registerHandler(PlayHandler(device, self.config["play"], self.actor_ref))
-        self.inputthread.registerHandler(PauseHandler(device, self.config["pause"], self.actor_ref))
-        self.inputthread.registerHandler(PlayPauseHandler(device, self.config["playpause"], self.actor_ref))
-        self.inputthread.registerHandler(VolumeUpHandler(device, self.config["volume_up"], self.actor_ref))
-        self.inputthread.registerHandler(VolumeDownHandler(device, self.config["volume_down"], self.actor_ref))
-        self.inputthread.registerHandler(NextSongHandler(device, self.config["next_song"], self.actor_ref))
-        self.inputthread.registerHandler(PreviousSongHandler(device, self.config["previous_song"], self.actor_ref))
-        self.inputthread.registerHandler(NextPlaylistHandler(device, self.config["next_playlist"], self.actor_ref))
-        self.inputthread.registerHandler(PreviousPlaylistHandler(device, self.config["previous_playlist"], self.actor_ref))
-        self.inputthread.registerHandler(MuteHandler(device, self.config["mute"], self.actor_ref))
+
+        self.inputthread = InputThread()
+        self.inputthread.register_handler(PlayHandler(device, self.config["play"], self.actor_ref))
+        self.inputthread.register_handler(PauseHandler(device, self.config["pause"], self.actor_ref))
+        self.inputthread.register_handler(PlayPauseHandler(device, self.config["playpause"], self.actor_ref))
+        self.inputthread.register_handler(VolumeUpHandler(device, self.config["volume_up"], self.actor_ref))
+        self.inputthread.register_handler(VolumeDownHandler(device, self.config["volume_down"], self.actor_ref))
+        self.inputthread.register_handler(NextSongHandler(device, self.config["next_song"], self.actor_ref))
+        self.inputthread.register_handler(PreviousSongHandler(device, self.config["previous_song"], self.actor_ref))
+        self.inputthread.register_handler(NextPlaylistHandler(device, self.config["next_playlist"], self.actor_ref))
+        self.inputthread.register_handler(
+            PreviousPlaylistHandler(device, self.config["previous_playlist"], self.actor_ref))
+        self.inputthread.register_handler(MuteHandler(device, self.config["mute"], self.actor_ref))
         self.inputthread.start()
 
     def on_stop(self):
         self.inputthread.stop()
 
-    def halt(self):
-        print("Goodbye")
-
     def change_volume(self, value):
-      volume=self.core.playback.volume.get()+value
-      if volume<0:
-          volume=0
-      elif volume>100:
-          volume=100
-          
-      logger.debug("Volume changed: {0}".format(volume))
-      self.core.playback.volume=volume
-    
+        volume = self.core.playback.volume.get() + value
+        if volume < 0:
+            volume = 0
+        elif volume > 100:
+            volume = 100
+
+        logger.debug("Volume changed: {0}".format(volume))
+        self.core.playback.volume = volume
+
     def next_song(self):
         self.core.playback.next()
 
@@ -137,7 +142,7 @@ class InputFrontend(pykka.ThreadingActor, core.CoreListener):
         self.core.playback.pause()
 
     def playpause_toggle(self):
-        if self.core.playback.state.get()==core.PlaybackState.PLAYING:
+        if self.core.playback.state.get() == core.PlaybackState.PLAYING:
             self.core.playback.pause()
         else:
             self.core.playback.play()
--- mopidy_headless/input.py
+++ mopidy_headless/input.py
@@ -4,86 +4,83 @@ import logging
 
 import threading
 
-from evdev import ecodes, InputDevice, list_devices, categorize
+from evdev import ecodes, InputDevice, list_devices
 
 from select import select
 
 logger = logging.getLogger(__name__)
 
+
 class InputThread(threading.Thread):
-  def __init__(self):
-    super(InputThread, self).__init__()
-    self.name="Input Thread"
-    
-    self._stop=threading.Event()
-    self.devices_by_fn={}
-    self.devices_by_fd={}
-    self.handlers_by_fd={}
+    def __init__(self):
+        super(InputThread, self).__init__()
+        self.name = "Input Thread"
 
-  def stop(self):
-    self._stop.set()
-    logger.debug("Thread {0} is asked to stop".format(self.name))
+        self._stop = threading.Event()
+        self.devices_by_fn = {}
+        self.devices_by_fd = {}
+        self.handlers_by_fd = {}
 
+    def stop(self):
+        self._stop.set()
+        logger.debug("Thread {0} is asked to stop".format(self.name))
 
-  def registerHandler(self, handler):
-    if (handler.device_fn in self.devices_by_fn):
-      device=self.devices_by_fn[handler.device_fn]
-    else:
-      device=InputDevice(handler.device_fn)
-      self.devices_by_fn[handler.device_fn]=device
-      self.devices_by_fd[device.fd]=device
-      self.handlers_by_fd[device.fd]=[]
-
-    #Check if device has needed event
-    capabilities= device.capabilities()
-    if handler.event_type in capabilities:
-      for event_code in handler.event_codes:
-        if (event_code in capabilities[handler.event_type]):
-          self.handlers_by_fd[device.fd].append(handler)
+    def register_handler(self, handler):
+        if handler.device_fn in self.devices_by_fn:
+            device = self.devices_by_fn[handler.device_fn]
         else:
-          logger.warning('Event {0} not found in input device "{1}"'.format(ecodes.bytype[handler.event_type][handler.event_code], device.name))
-          return False
-    else:
-      logger.warning('Input device "{1}" has no capability {0}'.format(ecodes.EV[handler.event_type], device.name))
-      return False
+            device = InputDevice(handler.device_fn)
+            self.devices_by_fn[handler.device_fn] = device
+            self.devices_by_fd[device.fd] = device
+            self.handlers_by_fd[device.fd] = []
 
-    return True
-      
+        # Check if device has needed event
+        capabilities = device.capabilities()
+        if handler.event_type in capabilities:
+            for event_code in handler.event_codes:
+                if event_code in capabilities[handler.event_type]:
+                    self.handlers_by_fd[device.fd].append(handler)
+                else:
+                    logger.warning('Event {0} not found in input device "{1}"'.format(
+                        ecodes.bytype[handler.event_type][handler.event_code], device.name))
+                    return False
+        else:
+            logger.warning(
+                'Input device "{1}" has no capability {0}'.format(ecodes.EV[handler.event_type], device.name))
+            return False
 
-  def run(self):
-    while not self._stop.isSet():
-      r,w,x = select(self.devices_by_fd, [], [], 10)
-      for fd in r:
-        for event in self.devices_by_fd[fd].read():
-          for handler in self.handlers_by_fd[fd]:
-            handler.check(event)
-    logger.debug("Thread {0} stopped".format(self.name))
+        return True
+
+    def run(self):
+        while not self._stop.isSet():
+            r, w, x = select(self.devices_by_fd, [], [], 10)
+            for fd in r:
+                for event in self.devices_by_fd[fd].read():
+                    for handler in self.handlers_by_fd[fd]:
+                        handler.check(event)
+        logger.debug("Thread {0} stopped".format(self.name))
 
 
 class Handler(object):
-  def __init__(self, device_fn, event_type, event_codes):
-    self.device_fn=device_fn
+    def __init__(self, device_fn, event_type, event_codes):
+        self.device_fn = device_fn
 
-    if (event_type in ecodes.ecodes):
-      self.event_type=ecodes.ecodes[event_type]
-    else:
-      logger.error('Event type {0} unknown'.format(event_type))
+        if event_type in ecodes.ecodes:
+            self.event_type = ecodes.ecodes[event_type]
+        else:
+            logger.error('Event type {0} unknown'.format(event_type))
 
-    self.event_codes = list()
-    for event_code in event_codes:
-      if (event_code in ecodes.ecodes):
-        self.event_codes.append(ecodes.ecodes[event_code])
-      else:
-        logger.error('Event {0} not found for {1} events'.format(event_code, event_type))
+        self.event_codes = list()
+        for event_code in event_codes:
+            if event_code in ecodes.ecodes:
+                self.event_codes.append(ecodes.ecodes[event_code])
+            else:
+                logger.error('Event {0} not found for {1} events'.format(event_code, event_type))
 
-  def check(self, event):
-    if self.event_type == event.type:
-      if event.code in self.event_codes:
-        self.handle(event)
-    
-  def handle(self, event):
-   pass
+    def check(self, event):
+        if self.event_type == event.type:
+            if event.code in self.event_codes:
+                self.handle(event)
 
-
-
-
+    def handle(self, event):
+        pass
